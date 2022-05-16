b10cda993b8b247a38f5cf748e7ec324c594e66c
jgottfriedMSC <jgottfried@mainstorconcept.com>
2022-05-16 21:18:54 +0200

    Participant entity fields

---
+++ src/main/java/matms/domain/Participant.java
@@ -0,0 +1,13 @@
+package matms.domain;
+
+import java.util.List;
+
+public class Participant {
+
+	private String id;
+	private String lastName;
+	private String firstName;
+	private Adress adress;
+	private List<MartialArt> martialArt;
+	
+}
