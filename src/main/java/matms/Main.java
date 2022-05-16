d4dee87f0f31c76ef06cb5d109725d63649baacc
jgottfriedMSC <jgottfried@mainstorconcept.com>
2022-05-16 21:19:21 +0200

    first step main function

--- src/main/java/matms/Main.java
+++ src/main/java/matms/Main.java
@@ -1,9 +1,20 @@
 package matms;
 
+import java.util.UUID;
+
+import matms.domain.*;
+
 public class Main {
 
 	public static void main(String[] args) {
-		System.out.println("RUNNING.");
+		var newUser = User.builder().id(UUID.randomUUID().toString())
+									.username("Test")
+									.password("test1")
+									.lastName("Gott")
+									.firstName("Jotta")
+									.permission(Permission.PARTICIPANT)
+									.build();
+		System.out.println(newUser.toString());
 	}
 	
 }
