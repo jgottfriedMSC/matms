bd4c9cf4eacdccdfa116806a0732af0f761dfad5
jgottfriedMSC <jgottfried@mainstorconcept.com>
2022-05-16 21:18:12 +0200

    Martial Arts Tournament entity fields

---
+++ src/main/java/matms/domain/MartialArtsTournament.java
@@ -0,0 +1,17 @@
+package matms.domain;
+
+import java.util.List;
+
+public class MartialArtsTournament {
+
+	private String id;
+	private String name;
+	private Adress venue;
+	private Money entryFee;
+	private Money ticketPrice;
+	private TournamentMode tournamentMode;
+	private List<Participant> participants;
+	private MartialArt martialArt;
+	
+	
+}
