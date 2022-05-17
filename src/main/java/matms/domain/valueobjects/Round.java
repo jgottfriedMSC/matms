fd833bebc7cec4923981b369cbb3ad3c04e362d2
jgottfried <jgottfried@xps-7DLS4M2>
2022-05-17 13:41:53 +0200

    Define Round

---
+++ src/main/java/matms/domain/valueobjects/Round.java
@@ -0,0 +1,42 @@
+package matms.domain.valueobjects;
+
+import java.util.Objects;
+
+import matms.domain.entities.Participant;
+
+public final class Round {
+
+	private final Participant participant;
+	private final Participant opponent;
+	
+	public Round(final Participant participant, final Participant opponent) {
+		this.participant = participant;
+		this.opponent = opponent;
+	}
+	
+	public Participant getParticipant() {
+		return participant;
+	}
+	
+	public Participant getOpponent() {
+		return participant;
+	}
+	
+	public boolean equals(Object o) {
+		if (o == null || getClass() != o.getClass()) {
+			return false;
+		}
+		
+		Round round = (Round) o;
+		
+		if (Objects.equals(participant, round.participant) && Objects.equals(opponent, round.opponent)) {
+			return true;
+		} else {
+			return false;
+		}
+	}
+	
+	public int hashCode() {
+		return Objects.hash(participant, opponent);
+	}
+}
