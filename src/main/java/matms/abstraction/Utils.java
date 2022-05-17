21c28d6150f359f01393ae2a793a6510c01bac3c
jgottfried <jgottfried@xps-7DLS4M2>
2022-05-17 16:17:08 +0200

    Math Utils (fact + Log)

---
+++ src/main/java/matms/abstraction/Utils.java
@@ -0,0 +1,19 @@
+package matms.abstraction;
+
+public class Utils {
+
+	public static int factCalculator(int n) {
+		int fact = 1;
+		int i = 1;
+		while (i <= n) {
+			fact = fact * i;
+			i++;
+		}
+		return fact;
+	}
+	
+	public static double customLog(int base, int logNumber) {
+		return Math.log(logNumber) / Math.log(base);
+	}
+	
+}
