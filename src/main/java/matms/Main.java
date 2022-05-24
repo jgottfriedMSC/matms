e9e1da2d45cc0a8a2476e43e58d6ec8aafbbdbd2
jgottfried <jgottfried@xps-7DLS4M2.mshome.net>
2022-05-24 16:48:59 +0200

    main method with RoundRobinMode

--- src/main/java/matms/Main.java
+++ src/main/java/matms/Main.java
@@ -2,40 +2,40 @@ package matms;
 
 import java.util.ArrayList;
 import java.util.HashMap;
-import java.util.Iterator;
 import java.util.List;
 import java.util.Map;
 import java.util.UUID;
 
+import matms.application.KnockoutMode;
+import matms.application.RoundRobinMode;
 import matms.application.RoundService;
+import matms.application.TournamentMode;
 import matms.application.TournamentParticipationService;
 import matms.domain.MartialArt;
 import matms.domain.Permission;
 import matms.domain.aggregates.MartialArtsTournament;
-import matms.domain.entities.KnockoutMode;
 import matms.domain.entities.Participant;
-import matms.domain.entities.SwissSystemMode;
 import matms.domain.entities.User;
 import matms.domain.exceptions.AuthenticationException;
 import matms.domain.exceptions.InvalidWeightException;
 import matms.domain.exceptions.NoRoundsException;
+import matms.domain.exceptions.NoWinnerException;
 import matms.domain.exceptions.ParticipantNotFoundException;
 import matms.domain.valueobjects.Adress;
 import matms.domain.valueobjects.Id;
-import matms.domain.valueobjects.Round;
 import matms.domain.valueobjects.Weight;
 
 public class Main {
 
 	public static void main(String[] args)
-			throws InvalidWeightException, AuthenticationException, ParticipantNotFoundException, NoRoundsException {
+			throws InvalidWeightException, AuthenticationException, ParticipantNotFoundException, NoRoundsException, NoWinnerException {
 
 		Map<String, Participant> partic = new HashMap<>();
 		List<MartialArt> arts = new ArrayList<>();
 		List<MartialArt> otherArts = new ArrayList<>();
 		arts.add(MartialArt.BOX);
 		otherArts.add(MartialArt.AIKIDO);
-		for (int i = 0; i < 8; i++) {
+		for (int i = 0; i < 4; i++) {
 			Participant p = Participant.builder().id(new Id(UUID.randomUUID().toString())).lastName("Wurst" + i)
 					.firstName("Hans" + i).adress(new Adress("Germany", "WurststraÃŸe", 76187, i))
 					.weight(new Weight(80, "Kg")).payedFee(true).martialArts(arts).build();
@@ -50,35 +50,26 @@ public class Main {
 				.lastName("Gott").firstName("Jotta").permission(Permission.TRAINER).build();
 
 		MartialArtsTournament tournament = MartialArtsTournament.builder().id(new Id(UUID.randomUUID().toString()))
-				.name("BoxTournament").venue(new Adress("Germany", "Benzstraße", 76137, 50)).participants(partic)
+				.name("BoxTournament").venue(new Adress("Germany", "Benzstraï¿½e", 76137, 50)).participants(partic)
 				.martialArt(MartialArt.BOX).build();
 
 		TournamentParticipationService participationService = new TournamentParticipationService(tournament);
 		participationService.removeUnqualifiedParticipants();
 
-		SwissSystemMode swiss = new SwissSystemMode(tournament);
+		TournamentMode roundRobin = new RoundRobinMode(tournament);
 
-		Iterator<String> iterator = partic.keySet().iterator();
-		String[] keySet = new String[8];
-		int i = 0;
-		while (iterator.hasNext()) {
-			keySet[i]= iterator.next();
-			i++;
+		// Berechne Runde
+		// Ergebnisse abfangen
+		// In einer while-Schleife mit Iterator durchgehen bis letzte Runde gespielt
+		RoundService roundService = new RoundService(roundRobin);
+		
+		while (roundService.checkIfThereIsWinner()) {
+			roundService.playRound(roundService.getCurrentRound());
+			roundService.playRound(roundService.nextRound());
 		}
 		
-		RoundService roundService = new RoundService(tournament, swiss);
-		List<Round> rounds = roundService.initializeEncounters();
-	
-		Participant roundWinner1 = roundService.determineRoundWinner(rounds.get(roundService.getCurrentRound()), tournament.getParticipants().get(keySet[0]));
-		Participant nextWinner1 = roundService.determineRoundWinner(rounds.get(roundService.getCurrentRound()), tournament.getParticipants().get(keySet[2]));
-		roundService.createRound(roundWinner1, nextWinner1);
+		System.out.println("Tournament Winner: " + roundService.getTournamentWinner().getFirstName() + " " + roundService.getTournamentWinner().getLastName());
 		
-		Participant roundWinner2 = roundService.determineRoundWinner(rounds.get(roundService.getCurrentRound()), tournament.getParticipants().get(keySet[4]));
-		Participant nextWinner2 = roundService.determineRoundWinner(rounds.get(roundService.getCurrentRound()), tournament.getParticipants().get(keySet[6]));
-		roundService.createRound(roundWinner2, nextWinner2);
-				
-		System.out.println(roundService.getRounds().size());
-		System.out.println(roundService.getNumberOfRounds());
 		// TournamentCreationService tournamentCreationService = new
 		// TournamentCreationService(newUser);
 		// MartialArtsTournament tournament =
