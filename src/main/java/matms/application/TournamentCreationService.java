f98433ac1a88a6a05072809f2be5dbe5ca3c0486
jgottfriedMSC <jgottfried@mainstorconcept.com>
2022-05-26 15:38:50 +0200

    implemented TournamentCreationService for permitted user

--- src/main/java/matms/application/TournamentCreationService.java
+++ src/main/java/matms/application/TournamentCreationService.java
@@ -1,9 +1,16 @@
 package matms.application;
 
+import java.util.Map;
+import java.util.UUID;
+
+import matms.domain.MartialArt;
 import matms.domain.Permission;
 import matms.domain.aggregates.MartialArtsTournament;
+import matms.domain.entities.Participant;
 import matms.domain.entities.User;
 import matms.domain.exceptions.AuthenticationException;
+import matms.domain.valueobjects.Adress;
+import matms.domain.valueobjects.Id;
 
 public final class TournamentCreationService {
 
@@ -13,13 +20,16 @@ public final class TournamentCreationService {
 		this.user = user;
 	}
 	
-	public MartialArtsTournament createTournament() throws AuthenticationException {
+	public MartialArtsTournament createTournament(MartialArt martialArt, Map<String, Participant> participants) throws AuthenticationException {
+		MartialArtsTournament tournament;
 		if (user.getPermission() == Permission.TRAINER || user.getPermission() == Permission.ORGANIZER) {
-			// Create Tournament with Builder
+			tournament = MartialArtsTournament.builder().id(new Id(UUID.randomUUID().toString()))
+					.name("BoxTournament").venue(new Adress("Germany", "Benzstra�e", 76137, 50)).participants(participants)
+					.martialArt(martialArt).build();
 		} else {
 			throw new AuthenticationException("User not allowed to create tournament!");
 		}
-		return null;
+		return tournament;
 	}
 	
 }
