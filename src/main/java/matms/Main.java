b607ea4b3c7e295f4c55fb81c39009b2b38e8d29
jgottfriedMSC <jgottfried@mainstorconcept.com>
2022-05-27 09:53:51 +0200

    latest main method

--- src/main/java/matms/Main.java
+++ src/main/java/matms/Main.java
@@ -1,37 +1,69 @@
 package matms;
 
+import java.io.FileNotFoundException;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.UUID;
 
-import matms.application.KnockoutMode;
-import matms.application.RoundRobinMode;
-import matms.application.RoundService;
+import matms.adapters.InMemoryMartialArtsTournamentRepository;
+import matms.adapters.InMemoryParticipantRepository;
+import matms.adapters.InMemoryUserRepository;
+import matms.adapters.RoundService;
+import matms.application.CreateUser;
+import matms.application.ParticipantService;
 import matms.application.SwissSystemMode;
-import matms.application.TournamentCreationService;
-import matms.application.TournamentMode;
 import matms.application.TournamentParticipationService;
+import matms.application.TournamentService;
 import matms.domain.MartialArt;
 import matms.domain.Permission;
+import matms.domain.TournamentMode;
 import matms.domain.aggregates.MartialArtsTournament;
 import matms.domain.entities.Participant;
 import matms.domain.entities.User;
 import matms.domain.exceptions.AuthenticationException;
+import matms.domain.exceptions.InvalidAmountException;
 import matms.domain.exceptions.InvalidWeightException;
 import matms.domain.exceptions.NoRoundsException;
 import matms.domain.exceptions.NoWinnerException;
 import matms.domain.exceptions.ParticipantNotFoundException;
+import matms.domain.exceptions.TournamentAlreadyExistsException;
+import matms.domain.exceptions.TournamentNotFoundException;
+import matms.domain.exceptions.UserAlreadyExistsException;
+import matms.domain.repositories.MartialArtsTournamentRepository;
+import matms.domain.repositories.ParticipantRepository;
+import matms.domain.repositories.UserRepository;
 import matms.domain.valueobjects.Adress;
 import matms.domain.valueobjects.Id;
+import matms.domain.valueobjects.Money;
 import matms.domain.valueobjects.Weight;
+import matms.plugins.TextStatistics;
 
 public class Main {
 
 	public static void main(String[] args)
-			throws InvalidWeightException, AuthenticationException, ParticipantNotFoundException, NoRoundsException, NoWinnerException {
-
+			throws InvalidWeightException, AuthenticationException, ParticipantNotFoundException, NoRoundsException, NoWinnerException, FileNotFoundException, UserAlreadyExistsException, TournamentAlreadyExistsException, InvalidAmountException, TournamentNotFoundException {
+		
+		UserRepository userRepo = new InMemoryUserRepository();
+		ParticipantRepository participantRepo = new InMemoryParticipantRepository();
+		MartialArtsTournamentRepository martialArtsTournamentrepo = new InMemoryMartialArtsTournamentRepository();
+		
+		CreateUser createUser = new CreateUser(userRepo);
+		ParticipantService participantService = new ParticipantService(participantRepo);
+		
+		
+		var newUser = User.builder().id(new Id(UUID.randomUUID().toString())).username("Test").password("test1")
+				.lastName("Gottfried").firstName("Justin").permission(Permission.TRAINER).build();
+		
+		var newUser2 = User.builder().id(new Id(UUID.randomUUID().toString())).username("Test").password("test1")
+				.lastName("Gottfried").firstName("James").permission(Permission.PARTICIPANT).build();
+		
+		createUser.create(newUser);
+		createUser.create(newUser2);
+		
+		TournamentService tournamentService = new TournamentService(newUser2, martialArtsTournamentrepo);
+		
 		Map<String, Participant> partic = new HashMap<>();
 		List<MartialArt> arts = new ArrayList<>();
 		List<MartialArt> otherArts = new ArrayList<>();
@@ -47,26 +79,32 @@ public class Main {
 			partic.put(p.getId().getUuid(), p);
 			partic.put(p_not.getId().getUuid(), p_not);
 		}
-
-		var newUser = User.builder().id(new Id(UUID.randomUUID().toString())).username("Test").password("test1")
-				.lastName("Gott").firstName("Jotta").permission(Permission.TRAINER).build();
-
-		TournamentCreationService creationService = new TournamentCreationService(newUser);
 		
-		MartialArtsTournament tournament = creationService.createTournament(MartialArt.BOX, partic);
-
-		TournamentParticipationService participationService = new TournamentParticipationService(tournament);
+		var tournament = MartialArtsTournament.builder()
+				.id(new Id(UUID.randomUUID().toString()))
+				.name("FirstBoxTournament")
+				.venue(new Adress("Germany", "Stuttgarter Straﬂe", 66178, 15))
+				.entryFee(new Money(10, "EUR"))
+				.ticketPrice(new Money(15, "EUR"))
+				.participants(partic)
+				.martialArt(MartialArt.BOX)
+				.build();
+		
+		tournamentService.addTournament(newUser, tournament);
+		
+		TournamentParticipationService participationService = new TournamentParticipationService(tournamentService.getById(tournament.getId().getUuid()));
 		participationService.removeUnqualifiedParticipants();
 
-		TournamentMode swissMode = new SwissSystemMode(tournament);
-
+		TournamentMode swissMode = new SwissSystemMode(tournamentService.getById(tournament.getId().getUuid()));
 		RoundService roundService = new RoundService(swissMode);
 		
 		while (roundService.checkIfThereIsWinner()) {
 			roundService.playRound(roundService.nextRound());
 		}
 		Participant winner = roundService.getTournamentWinner();
-		System.out.println("Tournament Winner: " + winner.getFirstName() + " " + winner.getLastName());
+		
+		TextStatistics textStats = new TextStatistics(roundService, userRepo, martialArtsTournamentrepo);
+		textStats.createDataOutputFile();
 	}
 
 }
