72ce38a623a37c881e1ac5ee4a1a16f59f78056d
jgottfriedMSC <jgottfried@mainstorconcept.com>
2022-05-27 12:39:32 +0200

    tested getter (not good)

--- src/test/java/matms/domain/tests/AdressSpec.java
+++ src/test/java/matms/domain/tests/AdressSpec.java
@@ -1,6 +1,7 @@
 package matms.domain.tests;
 
 import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
 
 import org.junit.jupiter.api.Test;
 
@@ -10,20 +11,27 @@ import matms.domain.valueobjects.Id;
 public class AdressSpec {
 	
 	@Test
-	public void equalsNullTest() {
+	public void equalsNullAndFalseClassTest() {
 		Adress adress = new Adress(null, null, 0, 0);
 		assertFalse(adress.equals(null));
-	}
-	
-	@Test
-	public void equalsFalseClassTest() {
-		Adress adress = new Adress(null, null, 0, 0);
 		assertFalse(adress.equals(new Id(null)));
 	}
 	
 	@Test
-	public void equalsNullAndFalseClassTest() {
-		
+	public void equalsAttributeTest() {
+		Adress adress1 = new Adress("a", "b", 1, 1);
+		Adress adress2 = new Adress("b", "a", 2, 2);
+		Adress adress3 = new Adress("b", "a", 2, 2);
+		assertFalse(adress1.equals(adress2));
+		assertFalse(adress2.equals(adress1));
+		assertTrue(adress2.equals(adress3));
+	}
+	
+	@Test
+	public void getCountryTest() {
+		Adress adress = new Adress("a", "d", 1, 1);
+		String country = "a";
+		assertTrue(adress.getCountry().equals(country));
 	}
 	
 }
