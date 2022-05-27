740f30656f7fca21f4d1a3755c1347c0b0f21a5a
jgottfriedMSC <jgottfried@mainstorconcept.com>
2022-05-27 12:22:21 +0200

    thorough tests for MathUtils class

--- src/main/java/matms/abstraction/MathUtils.java
+++ src/main/java/matms/abstraction/MathUtils.java
@@ -5,6 +5,8 @@ import java.math.BigInteger;
 public class MathUtils {
 
 	public static BigInteger factCalculator(BigInteger n) {
+		if (n.intValue() < 0) throw new IllegalArgumentException();
+		if (n.intValue() == 0) return BigInteger.ONE;
 		BigInteger fact = BigInteger.ONE;
 		BigInteger i = BigInteger.TWO;
 		while (i.intValue() <= n.intValue()) {
@@ -15,7 +17,8 @@ public class MathUtils {
 	}
 	
 	public static BigInteger binomialCoefficient(BigInteger n, BigInteger k) {
-		BigInteger x = factCalculator(n.subtract(k)).multiply(k);
+		if (k.intValue() < 0 || n.intValue() < 0 || k.intValue() > n.intValue()) throw new IllegalArgumentException();
+		BigInteger x = factCalculator(k).multiply(factCalculator(n.subtract(k)));
 		return factCalculator(n).divide(x);
 	}
 	
--- src/test/java/matms/abstraction/tests/MathUtilsSpec.java
+++ src/test/java/matms/abstraction/tests/MathUtilsSpec.java
@@ -4,6 +4,8 @@ import static org.junit.Assert.assertThrows;
 import static org.junit.Assert.assertTrue;
 
 import java.math.BigInteger;
+import java.math.RoundingMode;
+import java.text.DecimalFormat;
 
 import org.junit.jupiter.api.Test;
 
@@ -19,4 +21,38 @@ public class MathUtilsSpec {
 		assertTrue(MathUtils.factCalculator(BigInteger.valueOf(99)).compareTo(new BigInteger("933262154439441526816992388562667004907159682643816214685929638952175999932299156089414639761565182862536979208272237582511852109168640000000000000000000000")) == 0);
 	}
 	
+	@Test
+	public void factCalculatorNegativeTest() {
+		assertThrows(IllegalArgumentException.class, () -> MathUtils.factCalculator(BigInteger.valueOf(-1)));
+	}
+	
+	@Test
+	public void binomialCoefficientTest() {
+		assertTrue(MathUtils.binomialCoefficient(BigInteger.valueOf(50), BigInteger.ZERO).compareTo(BigInteger.ONE) == 0);
+		assertTrue(MathUtils.binomialCoefficient(BigInteger.valueOf(100), BigInteger.valueOf(100)).compareTo(BigInteger.ONE) == 0);
+		assertTrue(MathUtils.binomialCoefficient(BigInteger.valueOf(20), BigInteger.valueOf(5)).compareTo(MathUtils.binomialCoefficient(BigInteger.valueOf(20), BigInteger.valueOf(15))) == 0);
+		assertTrue(BigInteger.valueOf(10).multiply(MathUtils.binomialCoefficient(BigInteger.valueOf(15), BigInteger.valueOf(10))).compareTo(BigInteger.valueOf(15).multiply(MathUtils.binomialCoefficient(BigInteger.valueOf(14), BigInteger.valueOf(9)))) == 0);
+	}
+	
+	@Test
+	public void binomialCoefficientNegativeTest() {
+		assertThrows(IllegalArgumentException.class, () -> MathUtils.binomialCoefficient(BigInteger.valueOf(-1), BigInteger.valueOf(0)));
+		assertThrows(IllegalArgumentException.class, () -> MathUtils.binomialCoefficient(BigInteger.valueOf(0), BigInteger.valueOf(-1)));
+	}
+	
+	@Test
+	public void binomialCoefficientSmallerAndGreaterThanTest() {
+		assertThrows(IllegalArgumentException.class, () -> MathUtils.binomialCoefficient(BigInteger.valueOf(2), BigInteger.valueOf(10)));
+	}
+	
+	@Test
+	public void customLogTest() {
+		DecimalFormat df = new DecimalFormat("#.####");
+		df.setRoundingMode(RoundingMode.CEILING);
+		
+		assertTrue(MathUtils.customLog(2, 128) == MathUtils.customLog(2, 4) + MathUtils.customLog(2, 32));
+		assertTrue(MathUtils.customLog(2, 2) == MathUtils.customLog(2, 8) - MathUtils.customLog(2, 4));
+		assertTrue(MathUtils.customLog(5, 390625) == 4 * MathUtils.customLog(5, 25));
+		assertTrue(df.format(MathUtils.customLog(10, 8)).equals(df.format(MathUtils.customLog(2, 8) / MathUtils.customLog(2, 10))));
+	}
 }
